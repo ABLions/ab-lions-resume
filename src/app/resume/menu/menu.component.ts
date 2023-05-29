6b534028b5ae6d63ac08523a39c32161b34c40c2
Anderson Bello <andersonbello139@gmail.com>
2023-05-29 05:05:58 -0500

    [AB] Carousel, spliter, table, services, model and cards are added

--- src/app/resume/menu/menu.component.ts
+++ src/app/resume/menu/menu.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MenuItem, MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.scss']
+  styleUrls: ['./menu.component.scss'],
+  providers: [MessageService]
 })
-export class MenuComponent {
+export class MenuComponent implements OnInit{
+  items!: MenuItem[];
 
+    constructor(private messageService: MessageService) {}
+
+    ngOnInit() {
+      this.items = [
+          {
+              icon: 'pi pi-pencil',
+              command: () => {
+                  this.messageService.add({ severity: 'info', summary: 'Add', detail: 'Data Added' });
+              }
+          },
+          {
+              icon: 'pi pi-refresh',
+              command: () => {
+                  this.messageService.add({ severity: 'success', summary: 'Update', detail: 'Data Updated' });
+              }
+          },
+          {
+              icon: 'pi pi-trash',
+              command: () => {
+                  this.messageService.add({ severity: 'error', summary: 'Delete', detail: 'Data Deleted' });
+              }
+          },
+          {
+              icon: 'pi pi-upload',
+              routerLink: ['/fileupload']
+          },
+          {
+              icon: 'pi pi-external-link',
+              target: '_blank',
+              url: 'https://www.linkedin.com/in/anderbell7/'
+          }
+      ];
+    }
 }
--- src/app/resume/skills/skills.component.ts
+++ src/app/resume/skills/skills.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ProductService } from 'src/app/services/product.service';
+import { Product } from 'src/domain/product';
 
 @Component({
   selector: 'app-skills',
@@ -6,5 +8,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./skills.component.scss']
 })
 export class SkillsComponent {
+  products!: Product[];
 
+  responsiveOptions!: any[];
+
+  constructor(private productService: ProductService) {}
+
+  ngOnInit() {
+      this.productService.getProductsSmall().then((products) => {
+          this.products = products;
+      });
+
+      this.responsiveOptions = [
+          {
+              breakpoint: '1199px',
+              numVisible: 1,
+              numScroll: 1
+          },
+          {
+              breakpoint: '991px',
+              numVisible: 2,
+              numScroll: 1
+          },
+          {
+              breakpoint: '767px',
+              numVisible: 1,
+              numScroll: 1
+          }
+      ];
+  }
+
+  getSeverity(status: string | undefined): string {
+    if (status) {
+      return status.toUpperCase();
+    }
+    switch (status) {
+        case 'INSTOCK':
+            return 'success';
+        case 'LOWSTOCK':
+            return 'warning';
+        case 'OUTOFSTOCK':
+            return 'danger';
+        default:
+            return 'info';
+    }
+  }
 }
