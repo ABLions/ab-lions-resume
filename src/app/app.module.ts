3c8271218a336acb42fd03b412ae1a6be061e7cc
Anderson Bello <andersonbello@Andersons-MacBook-Pro.local>
2023-05-28 22:37:51 -0500

    basic structure ab-lions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutMeComponent } from './resume/about-me/about-me.component';
+import { WorkExperienceComponent } from './resume/work-experience/work-experience.component';
+import { CompanyLinksComponent } from './resume/company-links/company-links.component';
+import { SkillsComponent } from './resume/skills/skills.component';
+import { LanguagesComponent } from './resume/languages/languages.component';
+import { EducationComponent } from './resume/education/education.component';
+import { CertificationsComponent } from './resume/certifications/certifications.component';
+import { FooterComponent } from './resume/footer/footer.component';
+import { MenuComponent } from './resume/menu/menu.component';
+import { HomeComponent } from './resume/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutMeComponent,
+    WorkExperienceComponent,
+    CompanyLinksComponent,
+    SkillsComponent,
+    LanguagesComponent,
+    EducationComponent,
+    CertificationsComponent,
+    FooterComponent,
+    MenuComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/resume/company-links/company-links.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-company-links',
+  templateUrl: './company-links.component.html',
+  styleUrls: ['./company-links.component.scss']
+})
+export class CompanyLinksComponent {
+
+}
